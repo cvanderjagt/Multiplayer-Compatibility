a89d1520bafa52898f75fda179c0b98008670f01
cvanderjagt <cvanderjag@naviscrm.com>
2020-04-24 14:47:22 -0700

    Initial CE compatibility

---
+++ Source/Mods/CombatExtended.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+using HarmonyLib;
+
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Combat Extended by NoImageAvailable</summary>
+    /// <remarks>Incomplete</remarks>
+    /// <see href="https://steamcommunity.com/workshop/filedetails/?id=1631756268"/>
+    /// <see href="https://github.com/NoImageAvailable/CombatExtended"/>
+    [MpCompatFor("Combat Extended")]
+    public class CombatExtendedCompat
+    {
+        public CombatExtendedCompat(ModContentPack mod)
+        {
+            LongEventHandler.ExecuteWhenFinished(LateLoad);
+        }
+
+        static void LateLoad()
+        {
+            Type type;
+
+            {
+                type = AccessTools.TypeByName("CombatExtended.JobGiver_TakeAndEquip");
+                MP.RegisterSyncMethod(type, "TryGiveJob");
+
+                type = AccessTools.TypeByName("CombatExtended.CompAmmoUser");
+                MP.RegisterSyncMethod(type, "TryStartReload");
+
+                type = AccessTools.TypeByName("CombatExtended.Command_VerbTarget");
+                MP.RegisterSyncMethod(type, "ProcessInput");
+
+                type = AccessTools.TypeByName("CombatExtended.Update_Loadout");
+                MP.RegisterSyncMethod(type, "TryGiveJob");
+
+                type = AccessTools.TypeByName("CombatExtended.Building_TurretGunCE");
+                MP.RegisterSyncMethod(type, "TryStartShootSomething");
+
+                type = AccessTools.TypeByName("CombatExtended.CompFireModes");
+
+                var methods = new[]
+                {
+                    "ToggleAimMode",
+                    "ToggleFireMode",
+                    "ResetModes"
+                };
+
+                foreach (var method in methods)
+                {
+                    MP.RegisterSyncMethod(type, method);
+                }
+
+            }
+
+        
+
+        // RNG prevention
+            {
+                var methods = new[] {
+                    AccessTools.Method("CombatExtended.CompSuppressable:AddSuppression"),
+                    AccessTools.Method("CombatExtended.CompSuppressable:CompTick"),
+                    AccessTools.Method("CombatExtended.Verb_ShootCE:TryCastShot"),
+                    AccessTools.Method("CombatExtended.Verb_ShootCE:WarmupComplete"),
+                    AccessTools.Method("CombatExtended.HediffComp_InfecterCE:CheckMakeInfection"),
+                    AccessTools.Method("CombatExtended.ArmorUtilityCE:ApplyParryDamage"),
+                    AccessTools.Method("CombatExtended.ProjectileCE:TryCollideWith"),
+                    AccessTools.Method("CombatExtended.Verb_LaunchProjectileCE:WarmupComplete"),
+                    AccessTools.Method("CombatExtended.WeatherTracker:MapComponentTick"),
+                    AccessTools.Method("CombatExtended.JobGiver_TakeAndEquip:TryGiveJob"),
+                };
+
+                foreach(var method in methods) {
+                    MpCompat.harmony.Patch(method,
+                        prefix: new HarmonyMethod(typeof(CombatExtendedCompat), nameof(FixRNGPre)),
+                        postfix: new HarmonyMethod(typeof(CombatExtendedCompat), nameof(FixRNGPost))
+                    );
+                }
+            }
+        }
+        static void FixRNGPre()
+        {
+            Rand.PushState(Find.TickManager.TicksAbs);
+        }
+        static void FixRNGPost()
+        {
+            Rand.PopState();
+        }
+    }
+}
+
+
+
+    // Float Menus
+        /*{
+            type = AccessTools.TypeByName("CombatExtended.HarmonyCE.FloatMenuMakerMap_Modify_AddHumanlikeOrders");
+
+            // CE_Stabilizing
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass1_1", "<AddMenuItems>b__1");
+
+            // PickUp
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass1_2", "<AddMenuItems>b__4");
+
+            // PickUpAll
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass1_2", "<AddMenuItems>b__5");
+
+            // PickUpSome
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass1_2", "<AddMenuItems>b__7");
+
+            // Dialog_Slider
+            MP.RegisterSyncDelegate(type, "<>c__DisplayClass1_3", "<AddMenuItems>b__6");
+        }
+            {
+                // Toggles
+                type = AccessTools.TypeByName("CombatExtended.CompFireModes");
+
+                MP.RegisterSyncMethod(type, "ResetModes");
+            }
+
+            // Turret Gizmos
+            {
+                type = AccessTools.TypeByName("CombatExtended.Building_TurretGunCE");
+
+            //MP.RegisterSyncMethod(type, "<GetGizmos>b__64_0");
+            //MP.RegisterSyncMethod(type, "<GetGizmos>b__64_1");
+
+            MP.RegisterSyncMethod(type, "TryOrderReload");
+        }*/
+
+        // Loadouts
+        /*{
+            type = AccessTools.TypeByName("CombatExtended.Utility_Loadouts");
+
+            MP.RegisterSyncMethod(type, "SetLoadout");
+            MP.RegisterSyncMethod(type, "SetLoadoutById");
+            MP.RegisterSyncMethod(type, "GenerateLoadoutFromPawn");
+        }
+        {
+            type = AccessTools.TypeByName("CombatExtended.Loadout");
+
+            MP.RegisterSyncMethod(type, "Copy", new SyncType[] { type });
+            MP.RegisterSyncMethod(type, "Copy");
+            MP.RegisterSyncMethod(type, "AddSlot");
+            MP.RegisterSyncMethod(type, "MoveSlot");
+            MP.RegisterSyncMethod(type, "RemoveSlot", new SyncType[] { typeof(int) });
+            MP.RegisterSyncMethod(type, "RemoveSlot", new SyncType[] { AccessTools.TypeByName("CombatExtended.LoadoutSlot") });
+        }
+
+        // iTab Inventory?
+        {
+            // TODO
+        }*/
